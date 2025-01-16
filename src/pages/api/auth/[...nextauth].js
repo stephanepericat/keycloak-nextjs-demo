ebf2f729bbafe4ea1c4bb71ee971a0d207adf32d
Stephane Pericat <shortwavlabs@Mac.attlocal.net>
2025-01-16 13:03:38 -0600

    added Authentik provider

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import KeycloakProvider from "next-auth/providers/keycloak"
+import AuthentikProvider from "next-auth/providers/authentik"
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -8,7 +9,12 @@ export const authOptions = {
       clientId: process.env.KEYCLOAK_ID,
       clientSecret: process.env.KEYCLOAK_SECRET,
       issuer: process.env.KEYCLOAK_ISSUER,
-    })
+    }),
+    AuthentikProvider({
+      clientId: process.env.AUTHENTIK_ID,
+      clientSecret: process.env.AUTHENTIK_SECRET,
+      issuer: process.env.AUTHENTIK_ISSUER,
+    }),
   ],
 }
 
